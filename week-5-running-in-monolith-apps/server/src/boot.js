29935e47779cada81fd05e810c59883501d7c42e
dovakeen118 <kegillis118@gmail.com>
2023-03-02 11:04:40 -0500

    WIP boot config and removing TS traces

--- week-5-running-in-monolith-apps/server/src/boot.js
+++ week-5-running-in-monolith-apps/server/src/boot.js
@@ -1,16 +1,18 @@
-import bootDevelopment from "./boot/environments/development"
-import bootTest from "./boot/environments/test"
-import bootModel from "./boot/model.cjs"
-// import connection from "./boot/model.cjs"
+import "./boot/environments/development"
+import "./boot/environments/test"
+// import bootDevelopment from "./boot/environments/development"
+// import bootTest from "./boot/environments/test"
+// import bootModel from "./boot/model.cjs"
+import connection from "./boot/model.cjs"
 
-// export { connection }
+export { connection }
 
-const boot = ({ skipModel = false }) =>
-  Promise.all([bootDevelopment(), bootTest()]).then(() => {
-    if (!skipModel) {
-      return bootModel()
-    }
-    return null
-  })
+// const boot = ({ skipModel = false }) =>
+//   Promise.all([bootDevelopment(), bootTest()]).then(() => {
+//     if (!skipModel) {
+//       return bootModel()
+//     }
+//     return null
+//   })
 
-export default boot
+// export default boot
--- week-5-running-in-monolith-apps/server/src/boot/model.cjs
+++ week-5-running-in-monolith-apps/server/src/boot/model.cjs
@@ -1,9 +1,9 @@
-const knex = require("knex");
-const objection = require("objection");
-const knexConfig = require("../../knexfile.cjs");
+const knex = require("knex")
+const objection = require("objection")
+const knexConfig = require("../../knexfile.cjs")
 
-const knexConnection = knex(knexConfig);
+const knexConnection = knex(knexConfig)
 
-objection.Model.knex(knexConnection);
+objection.Model.knex(knexConnection)
 
-module.exports = knexConnection;
+module.exports = knexConnection
