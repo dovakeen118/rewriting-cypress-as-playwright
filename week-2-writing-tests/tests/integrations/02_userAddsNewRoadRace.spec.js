3e5302d53519a3db5be992ffe68792c590756f7a
dovakeen118 <kegillis118@gmail.com>
2023-05-12 14:44:01 -0400

    add more options to week 2 playwright tests

--- week-2-writing-tests/tests/integrations/02_userAddsNewRoadRace.spec.js
+++ week-2-writing-tests/tests/integrations/02_userAddsNewRoadRace.spec.js
@@ -1,6 +1,6 @@
 // @ts-check
-import fs from 'fs'
-import { expect, test } from "@playwright/test";
+import fs from "fs"
+import { expect, test } from "@playwright/test"
 
 import newRoadRace from "../newRoadRace.js"
 import starterRoadRaces from "../starterRoadRaces.js"
@@ -11,36 +11,52 @@ const roadRacesTestFilePath = "roadRacesTest.json"
 
 test.describe("Road Races New", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto("/road-races/new");
+    await page.goto("/road-races/new")
   })
 
   test.use({
-    ignoreHTTPSErrors: true,
-  });
+    ignoreHTTPSErrors: true
+  })
 
-  test("adds a road race to the list upon submitting the form", async ({ page }) => {
+  test("adds a road race to the list upon submitting the form", async ({
+    page
+  }) => {
     // test filling in the form and submitting here
-    await page.getByLabel("Name:").fill(newRoadRace.name);
-    await page.getByLabel("Distance in Miles:").fill(newRoadRace.miles);
-    await page.getByLabel("Location:").fill(newRoadRace.location);
-    await page.getByRole("button", { name: "Save this Race!" }).click();
+    await page.getByLabel("Name:").fill(newRoadRace.name)
+    await page.getByLabel("Distance in Miles:").fill(newRoadRace.miles)
+    await page.getByLabel("Location:").fill(newRoadRace.location)
+
+    // await page.getByRole("button", { name: "Save this Race!" }).click();
+    await page.getByText("Save this Race!").click()
 
     await expect(page).toHaveURL("/road-races")
-    await expect(page.getByRole("listitem")).toHaveCount(3);
-    
+    await expect(page.getByRole("listitem")).toHaveCount(3)
+
     // option 1: find last element in list
     // docs: use with caution, opt for locator with strictness
     const newRace = page.getByRole("listitem").last()
-    await expect(newRace).toHaveText(`${newRoadRace.name} - ${newRoadRace.miles} Miles`)
-    
+    await expect(newRace).toHaveText(
+      `${newRoadRace.name} - ${newRoadRace.miles} Miles`
+    )
+
     // option 2
-    await expect(page.getByRole("listitem")).toHaveText([
-      "Disney Princess Half Marathon - 13.1 Miles", 
-      "Moab Trail Marathon - 26.2 Miles", 
-      "Pikes Peak Marathon - 26.2 Miles"
-    ]);
+    // await expect(page.getByRole("listitem")).toHaveText([
+    //   "Disney Princess Half Marathon - 13.1 Miles",
+    //   "Moab Trail Marathon - 26.2 Miles",
+    //   "Pikes Peak Marathon - 26.2 Miles"
+    // ])
+
+    // option 3
+    await expect(
+      page.getByText("Pikes Peak Marathon - 26.2 Miles")
+    ).toBeVisible()
+
+    // option 4
+    await expect(
+      page.getByText(`${newRoadRace.name} - ${newRoadRace.miles} Miles`)
+    ).toBeVisible()
   })
-  
+
   test.afterEach(() => {
     fs.writeFileSync(roadRacesTestFilePath, JSON.stringify(starterRoadRaces))
   })
